bd4e1384a8c3baba744b1437bd1240472c243d2e
Alberto Parravicini <albyratta@gmail.com>
2015-05-05 15:51:50 +0200

    Added escape hatch cards (is something missing?)

--- src/main/java/it/polimi/ingsw/cg_8/model/cards/Card.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/cards/Card.java
@@ -2,4 +2,7 @@ package it.polimi.ingsw.cg_8.model.cards;
 
 public abstract class Card {
 
+	public Card() {
+		
+	}
 }
--- src/main/java/it/polimi/ingsw/cg_8/model/cards/escapeHatchCards/GreenEhCard.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/cards/escapeHatchCards/GreenEhCard.java
@@ -2,4 +2,7 @@ package it.polimi.ingsw.cg_8.model.cards.escapeHatchCards;
 
 public class GreenEhCard extends EscapeHatchCard {
 
+	public GreenEhCard() {
+		super();
+	}
 }
