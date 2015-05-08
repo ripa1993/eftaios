bb64a4d9d03970cbc805fbf8bcb1ffa97735298c
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-08 17:57:54 +0200

    removed unused imports

--- src/test/java/it/polimi/ingsw/cg_8/model/map/GalileiMapTest.java
+++ src/test/java/it/polimi/ingsw/cg_8/model/map/GalileiMapTest.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.cg_8.model.map;
 
 import static org.junit.Assert.*;
-import it.polimi.ingsw.cg_8.model.map.creator.FermiCreator;
 import it.polimi.ingsw.cg_8.model.map.creator.GalileiCreator;
 import it.polimi.ingsw.cg_8.model.map.creator.MapCreator;
 import it.polimi.ingsw.cg_8.model.sectors.Coordinate;
