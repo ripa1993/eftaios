7941f6b14061b654260f1d6d36c574dcd398514f
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-08 18:17:55 +0200

    testing equals in MapProxy

--- src/test/java/it/polimi/ingsw/cg_8/model/map/MapProxyTest.java
+++ src/test/java/it/polimi/ingsw/cg_8/model/map/MapProxyTest.java
@@ -5,19 +5,39 @@ import it.polimi.ingsw.cg_8.model.map.creator.FermiCreator;
 import it.polimi.ingsw.cg_8.model.map.creator.GalvaniCreator;
 import it.polimi.ingsw.cg_8.model.map.creator.MapCreator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MapProxyTest {
 
+	MapCreator mc;
+	GameMap map;
+	MapProxy mp;
+	
+	@Before
+	public void init(){
+		mc = new GalvaniCreator();
+		map = mc.createMap();
+		mp = map.getMapProxy();
+	}
+	
 	@Test
 	public void equals() {
-		MapCreator mc1 = new GalvaniCreator();
 		MapCreator mc2 = new FermiCreator();
-		GameMap map1 = mc1.createMap();
 		GameMap map2 = mc2.createMap();
-		MapProxy mp1 = map1.getMapProxy();
 		MapProxy mp2 = map2.getMapProxy();
-		assertFalse(mp1.equals(mp2));
+		assertFalse(mp.equals(mp2));
+	}
+	
+	@Test
+	public void equals2(){
+		GameMap map2 = mp.getMap();
+		MapProxy mp2 = map2.getMapProxy();
+		assertTrue(mp.equals(mp2));
 	}
 
+	@Test
+	public void getMap(){
+		assertEquals(map, mp.getMap());
+	}
 }
