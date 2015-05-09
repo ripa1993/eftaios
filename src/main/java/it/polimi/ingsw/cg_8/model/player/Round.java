bb49746307df5cf288d5dce5d266eef958e5a86f
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-09 17:31:52 +0200

    Added equals and hashcode

--- src/main/java/it/polimi/ingsw/cg_8/model/player/Round.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/player/Round.java
@@ -21,6 +21,37 @@ public class Round {
 	public Coordinate getCoordinate() {
 		return coordinate;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((coordinate == null) ? 0 : coordinate.hashCode());
+		result = prime * result + number;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Round other = (Round) obj;
+		if (coordinate == null) {
+			if (other.coordinate != null)
+				return false;
+		} else if (!coordinate.equals(other.coordinate))
+			return false;
+		if (number != other.number)
+			return false;
+		return true;
+	}
 	
 	
 }
