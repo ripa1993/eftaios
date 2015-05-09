d769d09b52922cd94b1864b067efc0d52f75ebb5
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-09 11:15:55 +0200

    added hashcode and equals, getters and setters

--- src/main/java/it/polimi/ingsw/cg_8/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/player/Player.java
@@ -4,6 +4,48 @@ import it.polimi.ingsw.cg_8.model.player.character.InGameCharacter;
 import it.polimi.ingsw.cg_8.model.sectors.Coordinate;
 
 public class Player {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((character == null) ? 0 : character.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((position == null) ? 0 : position.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (character == null) {
+			if (other.character != null)
+				return false;
+		} else if (!character.equals(other.character))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (state != other.state)
+			return false;
+		return true;
+	}
+
 	private PlayerState state;
 	private final String name;
 	private InGameCharacter character;
--- src/main/java/it/polimi/ingsw/cg_8/model/player/character/human/Human.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/player/character/human/Human.java
@@ -11,6 +11,33 @@ public class Human extends InGameCharacter {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime
+				* result
+				+ ((currentBehaviour == null) ? 0 : currentBehaviour.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Human other = (Human) obj;
+		if (currentBehaviour == null) {
+			if (other.currentBehaviour != null)
+				return false;
+		} else if (!currentBehaviour.equals(other.currentBehaviour))
+			return false;
+		return true;
+	}
+
+	@Override
 	public boolean isAttackAllowed() {
 		return currentBehaviour.isAttackAllowed();
 	}
