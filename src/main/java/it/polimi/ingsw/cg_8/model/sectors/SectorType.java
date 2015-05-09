b8aa4daab47457e6980a3f7828e6bdee820e60ca
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-09 16:54:24 +0200

    Removed sectorType enum and getSectorType method

--- src/main/java/it/polimi/ingsw/cg_8/model/sectors/SectorType.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.ingsw.cg_8.model.sectors;
-
-public enum SectorType {
-	DANGEROUS_SECTOR, SECURE_SECTOR, HUMAN_SECTOR, ALIEN_SECTOR, EH_SECTOR;
-}
--- src/main/java/it/polimi/ingsw/cg_8/model/sectors/special/spawn/AlienSector.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/sectors/special/spawn/AlienSector.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.cg_8.model.sectors.special.spawn;
 
-import it.polimi.ingsw.cg_8.model.sectors.SectorType;
 
 public class AlienSector extends SpawnSector {
 
@@ -9,9 +8,6 @@ public class AlienSector extends SpawnSector {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public SectorType getSectorType() {
-		return SectorType.ALIEN_SECTOR;
-	}
+	
 
 }
