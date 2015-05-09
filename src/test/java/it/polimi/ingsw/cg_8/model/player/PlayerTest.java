02fc085713fc9fa93c3b3b1afe165c22aa5dc2e7
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-09 12:07:03 +0200

    added player tests

---
+++ src/test/java/it/polimi/ingsw/cg_8/model/player/PlayerTest.java
@@ -0,0 +1,73 @@
+package it.polimi.ingsw.cg_8.model.player;
+
+import static org.junit.Assert.*;
+import it.polimi.ingsw.cg_8.model.player.character.InGameCharacter;
+import it.polimi.ingsw.cg_8.model.player.character.alien.Alien;
+import it.polimi.ingsw.cg_8.model.sectors.Coordinate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+	Player player;
+	InGameCharacter alien;
+	@Before
+	public void init(){
+		player = new Player("test");
+		alien = new Alien(player);
+		player.init(alien, new Coordinate(0,0));
+	}
+	
+	
+	@Test
+	public void testGetPosition() {
+		assertEquals(new Coordinate(0,0), player.getPosition());
+		
+	}
+
+	@Test
+	public void testSetPosition() {
+		player.setPosition(new Coordinate(1,1));
+		assertEquals(new Coordinate(1,1), player.getPosition());
+	}
+
+	@Test
+	public void testGetState() {
+		assertEquals(PlayerState.ALIVE_WAITING, player.getState());
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals("test", player.getName());
+	}
+
+	@Test
+	public void testGetCharacter() {
+		assertEquals(alien, player.getCharacter());
+	}
+
+	@Test
+	public void testSetDisconnected() {
+		player.setDisconnected();
+		assertEquals(PlayerState.DISCONNECTED, player.getState());
+	}
+
+	@Test
+	public void testSetDead() {
+		player.setDead();
+		assertEquals(PlayerState.DEAD, player.getState());
+	}
+
+	@Test
+	public void testCycleState() {
+		player.cycleState();
+		assertEquals(PlayerState.ALIVE_PLAYING, player.getState());
+	}
+	@Test
+	public void testCycleState2(){
+		player.cycleState();
+		player.cycleState();
+		assertEquals(PlayerState.ALIVE_WAITING, player.getState());
+	}
+
+}
