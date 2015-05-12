02efbfc5d83b5e8769e25b96e4770061793296e9
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-12 15:14:48 +0200

    added tests for escapehatchdeck and creator

---
+++ src/test/java/it/polimi/ingsw/cg_8/model/decks/EscapeHatchDeckTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.cg_8.model.decks;
+
+import static org.junit.Assert.*;
+import it.polimi.ingsw.cg_8.model.cards.escapeHatchCards.EscapeHatchCard;
+import it.polimi.ingsw.cg_8.model.decks.deckCreators.EscapeHatchDeckCreator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EscapeHatchDeckTest {
+	
+	EscapeHatchDeck cards;
+	EscapeHatchDeckCreator creator;
+	@Before
+	public void init(){
+		creator = new EscapeHatchDeckCreator();
+		cards = creator.createDeck();
+	}
+	
+	
+	@Test
+	public void testConstructor() {
+		assertTrue(cards.drawCard() instanceof EscapeHatchCard);
+	}
+
+}
