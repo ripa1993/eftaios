3e7665aa9a5f329606bc3fb56737ca5058638033
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-12 15:47:13 +0200

    Removed unused method

--- src/main/java/it/polimi/ingsw/cg_8/model/decks/deckCreators/DeckCreator.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/decks/deckCreators/DeckCreator.java
@@ -1,14 +1,11 @@
 package it.polimi.ingsw.cg_8.model.decks.deckCreators;
 
-import it.polimi.ingsw.cg_8.model.cards.Card;
 import it.polimi.ingsw.cg_8.model.decks.Deck;
-
-import java.util.List;
 
 public abstract class DeckCreator {
 
 	public abstract Deck createDeck();
 	
-	protected abstract List<Card> createCardList();
+	
 	
 }
