0850b73796073afc86ab89122387826a06d5fb9f
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-16 11:43:36 +0200

    Javadoc

--- src/main/java/it/polimi/ingsw/cg_8/model/sectors/Sector.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/sectors/Sector.java
@@ -1,8 +1,16 @@
 package it.polimi.ingsw.cg_8.model.sectors;
-
+/**
+ * Abstract sector class, every sector extends this class
+ * @author Simone
+ *
+ */
 
 public abstract class Sector extends Coordinate {
-
+/**
+ * Default constructor for sector
+ * @param x column number, first column is column 0
+ * @param y	row number, first row is row 0
+ */
 	public Sector(int x, int y) {
 		super(x, y);
 	}
