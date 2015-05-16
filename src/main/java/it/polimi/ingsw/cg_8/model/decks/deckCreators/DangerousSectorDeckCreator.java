86059689c52af5a2bbd3504110e2f65e84ea4b13
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-16 09:57:40 +0200

    Fixed naming convenctions for constants

--- src/main/java/it/polimi/ingsw/cg_8/model/decks/deckCreators/DangerousSectorDeckCreator.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/decks/deckCreators/DangerousSectorDeckCreator.java
@@ -8,29 +8,29 @@ import it.polimi.ingsw.cg_8.model.decks.DangerousSectorDeck;
 
 public class DangerousSectorDeckCreator extends DeckCreator {
 
-	private static final int silenceNum = 5;
-	private static final int noiseItemNum = 4;
-	private static final int noiseNoItemNum = 6;
-	private static final int fakeNoiseItemNum = 4;
-	private static final int fakeNoiseNoItemNum = 6;
+	private static final int SILENCE_NUM = 5;
+	private static final int NOISE_ITEM_NUM = 4;
+	private static final int NOISE_NO_ITEM_NUM = 6;
+	private static final int FAKE_NOISE_ITEM_NUM = 4;
+	private static final int FAKE_NOISE_NO_ITEM_NUM = 6;
 	@Override
 	public DangerousSectorDeck createDeck() {
 
 		DangerousSectorDeck dangDeck = new DangerousSectorDeck();
 		
-		for (int i = 0; i < silenceNum; i++) {
+		for (int i = 0; i < SILENCE_NUM; i++) {
 			dangDeck.addCard(new SilenceCard());
 		}
-		for (int i = 0; i < noiseItemNum; i++) {
+		for (int i = 0; i < NOISE_ITEM_NUM; i++) {
 			dangDeck.addCard(new NoiseWithItem(new NormalNoise()));
 		}
-		for (int i = 0; i < noiseNoItemNum; i++) {
+		for (int i = 0; i < NOISE_NO_ITEM_NUM; i++) {
 			dangDeck.addCard(new NormalNoise());
 		}	
-		for (int i = 0; i < fakeNoiseItemNum; i++) {
+		for (int i = 0; i < FAKE_NOISE_ITEM_NUM; i++) {
 			dangDeck.addCard(new NoiseWithItem(new FakeNoise(new NormalNoise())));
 		}
-		for (int i = 0; i < fakeNoiseNoItemNum; i++) {
+		for (int i = 0; i < FAKE_NOISE_NO_ITEM_NUM; i++) {
 			dangDeck.addCard(new FakeNoise(new NormalNoise()));
 		}
 		
--- src/main/java/it/polimi/ingsw/cg_8/model/decks/deckCreators/ItemDeckCreator.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/decks/deckCreators/ItemDeckCreator.java
@@ -11,29 +11,29 @@ import it.polimi.ingsw.cg_8.model.decks.ItemDeck;
 public class ItemDeckCreator extends DeckCreator {
 
 	
-	private static final int attCardNum = 2;
-	private static final int telCardNum = 2;
-	private static final int adrCardNum = 2;
-	private static final int sedCardNum = 3;
-	private static final int lightCardNum = 2;
+	private static final int ATTACK_NUM = 2;
+	private static final int TELEPORT_NUM = 2;
+	private static final int ADRENALINE_NUM = 2;
+	private static final int SEDATIVES_NUM = 3;
+	private static final int LIGHTS_NUM = 2;
 	
 	@Override
 	public ItemDeck createDeck() {
 		
 		ItemDeck itemDeck = new ItemDeck();
-		for (int i = 0; i < attCardNum; i++) {
+		for (int i = 0; i < ATTACK_NUM; i++) {
 			itemDeck.addCard(new AttackCard());
 		}
-		for (int i = 0; i < telCardNum; i++) {
+		for (int i = 0; i < TELEPORT_NUM; i++) {
 			itemDeck.addCard(new TeleportCard());
 		}
-		for (int i = 0; i < adrCardNum; i++) {
+		for (int i = 0; i < ADRENALINE_NUM; i++) {
 			itemDeck.addCard(new AdrenalineCard());
 		}
-		for (int i = 0; i < sedCardNum; i++) {
+		for (int i = 0; i < SEDATIVES_NUM; i++) {
 			itemDeck.addCard(new SedativesCard());
 		}
-		for (int i = 0; i < lightCardNum; i++) {
+		for (int i = 0; i < LIGHTS_NUM; i++) {
 			itemDeck.addCard(new SpotlightCard());
 		}
 		itemDeck.addCard(new DefenseCard()); // There is only one Defense Card in the ItemDeck
