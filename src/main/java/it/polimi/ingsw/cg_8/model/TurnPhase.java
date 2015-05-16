03da20ae466216226d825e52772baa259f036a1f
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-16 11:28:05 +0200

    Javadoc

--- src/main/java/it/polimi/ingsw/cg_8/model/TurnPhase.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/TurnPhase.java
@@ -1,15 +1,32 @@
 package it.polimi.ingsw.cg_8.model;
 /**
- * GAME_SETUP: Phase in which players are joining the newly created game<br>
- * GAME_END: Phase in which the game is ended<br>
- * TURN_BEGIN: Beginning of a player's turn<br>
- * MOVEMENT_PHASE: Player is choosing where to move<br>
- * ATTACK_PHASE: Player is choosing where to attack<br>
- * TURN_END: Player has finished his turn<br>
- * 
+ * Phases in a game's turn
  * @author Simone
  *
  */
 public enum TurnPhase {
-	GAME_SETUP, TURN_BEGIN, MOVEMENT_PHASE, ATTACK_PHASE, TURN_END, GAME_END;
+	/**
+	 * Phase in which players are joining the newly created game
+	 */
+	GAME_SETUP,
+	/**
+	 * Beginning of a player's turn
+	 */
+	TURN_BEGIN,
+	/**
+	 * Player is choosing where to move
+	 */
+	MOVEMENT_PHASE,
+	/**
+	 * Player is choosing where to attack
+	 */
+	ATTACK_PHASE,
+	/**
+	 * Player has finished his turn
+	 */
+	TURN_END,
+	/**
+	 * Phase in which the game is ended
+	 */
+	GAME_END;
 }
