d9014225fcd05247152a7566165cfa6fa22d5583
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-17 09:27:56 +0200

    Javadoc and toString

--- src/main/java/it/polimi/ingsw/cg_8/model/sectors/normal/SecureSector.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/sectors/normal/SecureSector.java
@@ -1,12 +1,29 @@
 package it.polimi.ingsw.cg_8.model.sectors.normal;
 
+/**
+ * Secure sector. When a player ends his turn in this sector he can safely
+ * finish his turn with no noises.
+ * 
+ * @author Simone
+ *
+ */
 
 public class SecureSector extends NormalSector {
-
+	/**
+	 * Secure sector constructor
+	 * 
+	 * @param x
+	 *            column number
+	 * @param y
+	 *            row number
+	 */
 	public SecureSector(int x, int y) {
-		super(x,y);
+		super(x, y);
 	}
 
-
+	@Override
+	public String toString() {
+		return "SecureSector "+super.toString();
+	}
 
 }
