4f3bc66dbffa3cafd3205afcd816d2f13231cd8a
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-17 10:49:40 +0200

    Javadoc and toString()

--- src/main/java/it/polimi/ingsw/cg_8/model/player/character/alien/Alien.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/player/character/alien/Alien.java
@@ -3,14 +3,60 @@ package it.polimi.ingsw.cg_8.model.player.character.alien;
 import it.polimi.ingsw.cg_8.model.cards.characterCards.CharacterCard;
 import it.polimi.ingsw.cg_8.model.player.character.InGameCharacter;
 
+/**
+ * Alien character
+ * 
+ * @author Simone
+ *
+ */
 public class Alien extends InGameCharacter {
+	/**
+	 * Implementation of strategy pattern. Current alien behaviour. The
+	 * {@link AlienBehaviour Behaviour} can be {@link NormalBehaviour Normal} or
+	 * {@link FedBehaviour Fed}
+	 */
 	private AlienBehaviour currentBehaviour;
-	
-	public Alien ( CharacterCard characterCard) {
-		super( characterCard);
+
+	/**
+	 * Constructor. By default the strategy is the {@link NormalBehaviour
+	 * Normal} one.
+	 * 
+	 * @param characterCard
+	 *            alien character card
+	 */
+	public Alien(CharacterCard characterCard) {
+		super(characterCard);
 		this.currentBehaviour = new NormalBehaviour();
 	}
-	
+
+	/**
+	 * Changes the current alien behaviour from {@link NormalBehaviour Normal}
+	 * to {@link FedBehaviour Fed}
+	 */
+	public void feedAlien() {
+		this.currentBehaviour = new FedBehaviour();
+	}
+
+	@Override
+	public int getMaxAllowedMovement() {
+		return currentBehaviour.getMaxMovementDistance();
+	}
+
+	@Override
+	public boolean isAttackAllowed() {
+		return true;
+	}
+
+	@Override
+	public boolean isDefendAllowed() {
+		return false;
+	}
+
+	@Override
+	public boolean hasToDrawSectorCard() {
+		return true;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -38,30 +84,13 @@ public class Alien extends InGameCharacter {
 		return true;
 	}
 
-	public void feedAlien(){
-		this.currentBehaviour = new FedBehaviour();
-	}
-	
 	@Override
-	public int getMaxAllowedMovement(){
-		return currentBehaviour.getMaxMovementDistance();
+	public String toString() {
+		return "Alien [currentBehaviour=" + currentBehaviour
+				+ ", getMaxAllowedMovement()=" + getMaxAllowedMovement()
+				+ ", isAttackAllowed()=" + isAttackAllowed()
+				+ ", isDefendAllowed()=" + isDefendAllowed()
+				+ ", hasToDrawSectorCard()=" + hasToDrawSectorCard() + "]";
 	}
 
-	@Override
-	public boolean isAttackAllowed() {
-		return true;
-	}
-
-	@Override
-	public boolean isDefendAllowed() {
-		return false;
-	}
-
-	@Override
-	public boolean hasToDrawSectorCard() {
-		return true;
-	}
-
-	
-	
 }
