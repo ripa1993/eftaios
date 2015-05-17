e0663f1293b2717d6c5a7722843609fc62a9ed3f
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-17 09:20:51 +0200

    Javadoc and toString added

--- src/main/java/it/polimi/ingsw/cg_8/model/sectors/special/escapehatch/NotUsedEHBehaviour.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/sectors/special/escapehatch/NotUsedEHBehaviour.java
@@ -1,13 +1,30 @@
 package it.polimi.ingsw.cg_8.model.sectors.special.escapehatch;
 
+/**
+ * Implementation of the not used escape hatch behaviour
+ * 
+ * @author Simone
+ *
+ */
 public class NotUsedEHBehaviour implements EscapeHatchBehaviour {
 
+	/**
+	 * Default constructor
+	 */
 	public NotUsedEHBehaviour() {
 	}
 
+	/**
+	 * Player is allowed to escape
+	 */
 	@Override
 	public boolean allowEscape() {
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "NotUsedEHBehaviour [allowEscape()=" + allowEscape() + "]";
 	}
 
 }
