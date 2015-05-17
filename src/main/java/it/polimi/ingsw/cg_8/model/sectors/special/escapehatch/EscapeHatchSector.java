4218b769f399c794b9c4d6149a48a1468f949027
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-17 09:27:46 +0200

    Corrected a typo in javadoc

--- src/main/java/it/polimi/ingsw/cg_8/model/sectors/special/escapehatch/EscapeHatchSector.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/sectors/special/escapehatch/EscapeHatchSector.java
@@ -87,7 +87,7 @@ public class EscapeHatchSector extends SpecialSector {
 	@Override
 	public String toString() {
 		return "EscapeHatchSector [status=" + status + ", number=" + number
-				+ "]";
+				+ "] " + super.toString();
 	}
 
 }
