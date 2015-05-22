7ba1fe94c9049843a07f709891806dfb3bdccad3
Alberto Parravicini <albyratta@gmail.com>
2015-05-22 19:19:25 +0200

    Reset the shield decoration on the attacked human

--- src/main/java/it/polimi/ingsw/cg_8/controller/playerActions/Attack.java
+++ src/main/java/it/polimi/ingsw/cg_8/controller/playerActions/Attack.java
@@ -76,7 +76,7 @@ public class Attack extends PlayerAction {
 					UseDefenseCard defense = new UseDefenseCard(model);
 					defense.useCard();
 					heldCards.getHeldCards().remove(c);
-					
+					p.resetDecorations();
 				}
 			}
 			if (p.getCharacter().isDefendAllowed() == false) {
