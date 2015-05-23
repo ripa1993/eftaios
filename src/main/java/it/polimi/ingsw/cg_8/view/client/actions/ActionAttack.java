de14ff5059a470a9765f782d71bb50c2b950e99b
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-23 15:25:31 +0200

    Added interface ClientAction and its implementetions as Action*

---
+++ src/main/java/it/polimi/ingsw/cg_8/view/client/actions/ActionAttack.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.cg_8.view.client.actions;
+
+import java.io.Serializable;
+/**
+ * Action: do an attack
+ * @author Simone
+ *
+ */
+public class ActionAttack implements ClientAction, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7008333309146096141L;
+
+	/**
+	 * Constructor
+	 */
+	public ActionAttack() {
+
+	}
+}
---
+++ src/main/java/it/polimi/ingsw/cg_8/view/client/actions/ActionEndTurn.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.cg_8.view.client.actions;
+
+import java.io.Serializable;
+
+/**
+ * Action: end turn
+ * 
+ * @author Simone
+ *
+ */
+public class ActionEndTurn implements ClientAction, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8940775774902250932L;
+
+	/**
+	 * Constructor
+	 */
+	public ActionEndTurn() {
+
+	}
+}
