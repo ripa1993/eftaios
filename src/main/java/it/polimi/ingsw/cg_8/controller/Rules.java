e457576b7fe6373002595dc77cb34b16abeeb60b
Alberto Parravicini <albyratta@gmail.com>
2015-05-23 09:57:50 +0200

    Changed Rules Description

--- src/main/java/it/polimi/ingsw/cg_8/controller/Rules.java
+++ src/main/java/it/polimi/ingsw/cg_8/controller/Rules.java
@@ -1,15 +1,14 @@
 package it.polimi.ingsw.cg_8.controller;
 
-
 /**
- * This class contains most of the game logic, in terms of rules and actions
- * allowed to the players. It is dependent on the model, as certain rules might
- * change according to the game state.
+ * This class is used to aggregate the ActionValidators, and its instance is
+ * passed to the controller so that it knows how to validate moves. It is
+ * dependent on the model, as certain rules might change according to the game
+ * state.
  * 
  * @author Alberto Parravicini
  *
  */
 public class Rules {
-
 
 }
