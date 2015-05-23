a3e2043de5b42f1ec6407c5442b77f0877ab886d
Alberto Parravicini <albyratta@gmail.com>
2015-05-23 12:02:01 +0200

    Tested UseItemCardValidator

---
+++ src/test/java/it/polimi/ingsw/cg_8/controller/playerActions/UsetItemCardValidatorTest.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.cg_8.controller.playerActions;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import it.polimi.ingsw.cg_8.model.Model;
+import it.polimi.ingsw.cg_8.model.cards.itemCards.AdrenalineCard;
+import it.polimi.ingsw.cg_8.model.cards.itemCards.AttackCard;
+import it.polimi.ingsw.cg_8.model.cards.itemCards.SedativesCard;
+import it.polimi.ingsw.cg_8.model.exceptions.EmptyDeckException;
+import it.polimi.ingsw.cg_8.model.exceptions.GameAlreadyRunningException;
+import it.polimi.ingsw.cg_8.model.exceptions.NotAValidMapException;
+import it.polimi.ingsw.cg_8.model.exceptions.TooManyCardsException;
+import it.polimi.ingsw.cg_8.model.map.GameMapName;
+import it.polimi.ingsw.cg_8.model.player.Player;
+import it.polimi.ingsw.cg_8.model.player.character.alien.Alien;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UsetItemCardValidatorTest {
+	Model model;
+	Player currentPlayer;
+
+	@Before
+	public void init() throws NotAValidMapException,
+			GameAlreadyRunningException, EmptyDeckException, TooManyCardsException {
+		model = new Model(GameMapName.FERMI);
+		model.addPlayer("Ann");
+		model.addPlayer("Bob");
+		model.initGame();
+		currentPlayer = model.getPlayers().get(model.getCurrentPlayer());
+		if (currentPlayer.getCharacter() instanceof Alien) {
+			model.nextPlayer();
+			currentPlayer = model.getPlayers().get(model.getCurrentPlayer());
+		}
+		currentPlayer.getHand().addItemCard(new AttackCard());
+		currentPlayer.getHand().addItemCard(new AdrenalineCard());		
+	}
+	
+	@Test
+	public void testCardUsage() {
+		assertTrue(UseItemCardValidator.validateItemCardUsage(model, new AdrenalineCard()));
+	}
+	
+	@Test
+	public void testCardUsage2() {
+		assertTrue(UseItemCardValidator.validateItemCardUsage(model, new AttackCard()));
+	}
+	@Test
+	public void testCardUsage3() {
+		assertFalse(UseItemCardValidator.validateItemCardUsage(model, new SedativesCard()));
+	}
+}
