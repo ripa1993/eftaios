6ef17a1336eded2e7652ae07c57ee941f4864fbe
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-24 15:54:38 +0200

    Added two more states

--- src/main/java/it/polimi/ingsw/cg_8/model/TurnPhase.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/TurnPhase.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.cg_8.model;
+
 /**
  * Phases in a game's turn
+ * 
  * @author Simone
  *
  */
@@ -9,27 +11,38 @@ public enum TurnPhase {
 	 * Phase in which players are joining the newly created game
 	 */
 	GAME_SETUP,
-	
+
 	/**
 	 * Beginning of a player's turn
 	 */
 	TURN_BEGIN,
-	
+
 	/**
 	 * Player is choosing where to move
 	 */
 	MOVEMENT_PHASE,
-	
+
 	/**
 	 * Player is choosing where to attack
 	 */
 	ATTACK_PHASE,
-	
+
+	/**
+	 * Waiting for the player to communicate location of fake noise, after
+	 * having drawn a Fake Noise card
+	 */
+	WAITING_FAKE_NOISE,
+
+	/**
+	 * Player has drawn a card after having moved.
+	 */
+	DRAWN_CARD,
+
 	/**
 	 * Player has finished his turn
 	 */
 	TURN_END,
-	
+
 	/**
 	 * Phase in which the game is ended
 	 */
