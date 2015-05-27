ec72772d604bfe9639c446db0575037f64c7ed55
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-27 20:47:56 +0200

    updates

---
+++ src/main/java/it/polimi/ingsw/cg_8/client/ClientData.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.cg_8.client;
+
+import it.polimi.ingsw.cg_8.view.server.ResponseChat;
+import it.polimi.ingsw.cg_8.view.server.ResponseNoise;
+import it.polimi.ingsw.cg_8.view.server.ResponsePrivate;
+import it.polimi.ingsw.cg_8.view.server.ServerResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientData {
+	private List<ResponseChat> chat;
+	private List<ResponseNoise> noise;
+	private List<ResponsePrivate> privateMessages;
+	
+	
+	public ClientData(){
+		chat = new ArrayList<ResponseChat>();
+		noise = new ArrayList<ResponseNoise>();
+		privateMessages = new ArrayList<ResponsePrivate>();	
+	}
+	
+	public void storeResponse(ServerResponse response){
+		if(response instanceof ResponseChat){
+			chat.add((ResponseChat) response);
+			// notify
+			return;
+		}
+		if(response instanceof ResponseNoise){
+			noise.add((ResponseNoise) response);
+			// notify
+			return;
+		}
+		if(response instanceof ResponsePrivate){
+			privateMessages.add((ResponsePrivate) response);
+			// notify
+			return;
+		}
+		return;
+	}
+}
--- src/main/java/it/polimi/ingsw/cg_8/client/ClientSocketViewSUB.java
+++ src/main/java/it/polimi/ingsw/cg_8/client/ClientSocketViewSUB.java
@@ -1,16 +1,18 @@
 package it.polimi.ingsw.cg_8.client;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class ClientSocketViewSUB implements Runnable {
 
-	Socket subSocket;
+	private Socket subSocket;
+	private ObjectInputStream input;
 
 	public ClientSocketViewSUB(String serverIP, int serverPubPort) {
 		try {
 			this.subSocket = new Socket(serverIP, serverPubPort);
-
+			this.input = new ObjectInputStream(subSocket.getInputStream());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -20,6 +22,10 @@ public class ClientSocketViewSUB implements Runnable {
 	@Override
 	public void run() {
 		System.out.println("Subscribed.");
+		while(true){
+			
+		}
+		
 	}
 
 }
