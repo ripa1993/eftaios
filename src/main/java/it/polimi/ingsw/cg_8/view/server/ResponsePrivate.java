ad35c6dcbf922c1a1e4683df4306c1cd39782635
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-05-27 16:12:53 +0200

    Added server responses

---
+++ src/main/java/it/polimi/ingsw/cg_8/view/server/ResponsePrivate.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.cg_8.view.server;
+
+import java.io.Serializable;
+
+public class ResponsePrivate implements Serializable, ServerResponse {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3728410205672503413L;
+	private final String message;
+	
+	public ResponsePrivate(String message){
+		this.message=message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
