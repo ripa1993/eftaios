2d107a6cf9f378e3d1fcc45b76fe13fd9140dfff
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-06-09 15:49:45 +0200

    ServerResponse interface and implementation javadoc

--- src/main/java/it/polimi/ingsw/cg_8/view/server/ResponseNoise.java
+++ src/main/java/it/polimi/ingsw/cg_8/view/server/ResponseNoise.java
@@ -5,19 +5,38 @@ import it.polimi.ingsw.cg_8.model.sectors.Coordinate;
 
 import java.io.Serializable;
 
+/**
+ * Server response used in order to comunicate Noises to the clients
+ * 
+ * @author Simone
+ *
+ */
 public class ResponseNoise implements Serializable, ServerResponse {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -539924760536211551L;
+	/**
+	 * Noise to be sent
+	 */
 	private final Noise noise;
 
+	/**
+	 * Builds a new server response with the given noise
+	 * 
+	 * @param noise
+	 *            noise to be communicated
+	 */
 	public ResponseNoise(Noise noise) {
 		this.noise = noise;
 	}
-	
-	public Noise getNoise(){
+
+	/**
+	 * 
+	 * @return the communicated noise
+	 */
+	public Noise getNoise() {
 		return noise;
 	}
 
