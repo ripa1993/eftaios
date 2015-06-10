ae0c3de72e6d9787979277a21509f4783522bee8
Simone Ripamonti <simone.ripamonti.1993@gmail.com>
2015-06-10 16:55:01 +0200

    Javadoc for model exceptions

--- src/main/java/it/polimi/ingsw/cg_8/model/exceptions/GameAlreadyRunningException.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/exceptions/GameAlreadyRunningException.java
@@ -1,7 +1,17 @@
 package it.polimi.ingsw.cg_8.model.exceptions;
 
+/**
+ * This exception is thrown when you try to add a player to an already started
+ * game
+ * 
+ * @author Simone
+ * @version 1.0
+ */
 public class GameAlreadyRunningException extends Exception {
-
+	/**
+	 * Creates an exception with message
+	 * @param string message
+	 */
 	public GameAlreadyRunningException(String string) {
 		super(string);
 	}
--- src/main/java/it/polimi/ingsw/cg_8/model/exceptions/NotAValidMapException.java
+++ src/main/java/it/polimi/ingsw/cg_8/model/exceptions/NotAValidMapException.java
@@ -1,7 +1,18 @@
 package it.polimi.ingsw.cg_8.model.exceptions;
 
+/**
+ * This exception is thrown when you try to create a game with a non valid map
+ * 
+ * @author Simone
+ * @version 1.0
+ */
 public class NotAValidMapException extends Exception {
-
+	/**
+	 * Creates an exception with message
+	 * 
+	 * @param string
+	 *            message
+	 */
 	public NotAValidMapException(String string) {
 		super(string);
 	}
