d4d8ee28b1d9e97046fc2da11ddf68763efd1543
Alberto Parravicini <albyratta@gmail.com>
2015-06-12 21:14:49 +0200

    Re-added card moving when pressed

--- src/main/java/it/polimi/ingsw/cg_8/client/gui/CardButton.java
+++ src/main/java/it/polimi/ingsw/cg_8/client/gui/CardButton.java
@@ -7,14 +7,14 @@ import it.polimi.ingsw.cg_8.model.cards.itemCards.SedativesCard;
 import it.polimi.ingsw.cg_8.model.cards.itemCards.SpotlightCard;
 import it.polimi.ingsw.cg_8.model.cards.itemCards.TeleportCard;
 import it.polimi.ingsw.cg_8.model.sectors.Coordinate;
-import it.polimi.ingsw.cg_8.server.Server;
 import it.polimi.ingsw.cg_8.view.client.ActionParser;
 import it.polimi.ingsw.cg_8.view.client.actions.ActionUseCard;
 import it.polimi.ingsw.cg_8.view.client.actions.ClientAction;
 import it.polimi.ingsw.cg_8.view.client.exceptions.NotAValidInput;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.CardLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Image;
@@ -33,6 +33,7 @@ import javax.swing.SwingConstants;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import javax.swing.border.EmptyBorder;
 
 /**
  * JButton that allows the player to use an item card.
@@ -43,19 +44,29 @@ import org.apache.logging.log4j.Logger;
 public class CardButton extends JPanel {
 
 	private static final long serialVersionUID = -4771580323012024149L;
-	private JLabel cardButton;
+	private JLabel cardButtonImage;
 	private JLabel text;
 	private CardType cardType;
+	private CardLayout cardLayout;
+	private JLabel cardButtonOverlay;
 	private Font fontTitilliumSemiboldUpright;
 	/**
 	 * Log4j logger
 	 */
 	private static final Logger logger = LogManager.getLogger(CardButton.class);
-	public CardButton() {
+	private JPanel cardButton;
 
-		this.cardButton = new JLabel();
+	public CardButton() {
 		this.text = new JLabel("No Card");
 		this.cardType = CardType.DEFAULT;
+		this.cardButton = new JPanel();
+		cardButton.setMaximumSize(new Dimension(150, 150));
+		cardButton.setOpaque(false);
+		this.cardButtonOverlay = new JLabel();
+		cardButtonOverlay.setBorder(new EmptyBorder(5, 0, 0, 0));
+
+		this.cardButtonImage = new JLabel();
+
 		try {
 			fontTitilliumSemiboldUpright = Font.createFont(
 					Font.TRUETYPE_FONT,
@@ -66,38 +77,58 @@ public class CardButton extends JPanel {
 			logger.error(e.getMessage());
 		}
 
-		setOpaque(false);
+		try {
+			Image tempOverlay = ImageIO.read(new File(
+					Resource.IMG_HUMAN_OVERLAY));
+			Image cardOverlay = tempOverlay.getScaledInstance(100, -1,
+					Image.SCALE_SMOOTH);
+			cardButtonOverlay.setIcon(new ImageIcon(cardOverlay));
+
+		} catch (IOException ex) {
+			logger.error(ex.getMessage());
+		}
+
+		this.setImage(Resource.IMG_ITEM);
 
 		this.setLayout(new BorderLayout());
 		this.text.setFont(fontTitilliumSemiboldUpright);
 
 		text.setHorizontalAlignment(SwingConstants.CENTER);
 
-		this.add(cardButton, BorderLayout.CENTER);
-		this.add(text, BorderLayout.SOUTH);
-		this.setImage(Resource.IMG_ITEM);
+		add(cardButton, BorderLayout.NORTH);
+		cardLayout = new CardLayout(0, 0);
+		cardButton.setLayout(cardLayout);
 
-		setOpaque(false);
-		this.setVisible(true);
-		cardButton.addMouseListener(new MouseAdapter() {
+		cardButtonOverlay.setOpaque(false);
+		cardButtonImage.setOpaque(false);
+
+		cardButton.add(cardButtonImage, "cardButtonImage");
+
+		cardButton.add(cardButtonOverlay, "cardButtonOverlay");
+	
+		cardButtonImage.addMouseListener(new MouseAdapter() {
 			int width, height;
 
 			@Override
-			public void mouseEntered(MouseEvent event) {
-				width = cardButton.getWidth();
-				height = cardButton.getHeight();
-				cardButton.setSize((int) (1.05 * width),
+			public void mousePressed(MouseEvent event) {
+				width = cardButtonImage.getWidth();
+				height = cardButtonImage.getHeight();
+				cardButtonImage.setSize((int) (1.05 * width),
 						((int) (1.05 * height)));
-				cardButton.repaint();
+				cardButtonImage.repaint();
 			}
 
 			@Override
-			public void mouseExited(MouseEvent event) {
-				cardButton.setSize(width, height);
-				cardButton.repaint();
+			public void mouseReleased(MouseEvent event) {
+				cardButtonImage.setSize(width, height);
+				cardButtonImage.repaint();
 			}
 
 		});
+		this.add(text, BorderLayout.SOUTH);
+
+		setOpaque(false);
+		this.setVisible(true);
 
 	}
 
@@ -123,42 +154,42 @@ public class CardButton extends JPanel {
 		if (this.cardType.equals(CardType.DEFAULT)) {
 			this.setImage(Resource.IMG_ITEM);
 			this.setText("No Card");
-			this.cardButton
+			this.cardButtonImage
 					.setToolTipText("<html>This is an <b>empty slot</b> for item card</html>");
 		} else if (this.cardType.equals(CardType.ADRENALINE)) {
 			this.setImage(Resource.IMG_ADRENALINE);
 			this.setText("Adrenaline");
-			this.cardButton
+			this.cardButtonImage
 					.setToolTipText("<html>This card allows you to <b>move two Sectors</b> this turn.</html>");
 		} else if (this.cardType.equals(CardType.ATTACK)) {
 			this.setImage(Resource.IMG_ATTACK);
 			this.setText("Attack");
-			this.cardButton
+			this.cardButtonImage
 					.setToolTipText("<html>This card allows you to <b>attack</b>, using the same rules as the Aliens.<br>"
 							+ "Note: the Human character can still move only one Sector.</html>");
 		} else if (this.cardType.equals(CardType.DEFENSE)) {
 			this.setImage(Resource.IMG_DEFENSE);
 			this.setText("Defense");
-			this.cardButton
+			this.cardButtonImage
 					.setToolTipText("<html>Play this card immediately when an Alien attacks you.<br>"
 							+ "You are  <b>not affected</b> by the attack.</html>");
 		} else if (this.cardType.equals(CardType.SEDATIVES)) {
 			this.setImage(Resource.IMG_SEDATIVES);
 			this.setText("Sedatives");
-			this.cardButton
+			this.cardButtonImage
 					.setToolTipText("<html>If you play this card  <b>you do not draw </b> a Dangerous Sector Card this turn,<br>"
 							+ "even if you move into a Dangerous Sector.</html>");
 		} else if (this.cardType.equals(CardType.SPOTLIGHT)) {
 			this.setImage(Resource.IMG_SPOTLIGHT);
 			this.setText("Spotlight");
-			this.cardButton
+			this.cardButtonImage
 					.setToolTipText("<html>When you play this card, name any Sector. Any players (including you)<br>"
 							+ "that are in the named Sector or in any of the six adjacent Sectors must immediately<br>"
 							+ " <b>announce their exact location </b> Coordinates. This card affects both Humans and Aliens.<html>");
 		} else if (this.cardType.equals(CardType.TELEPORT)) {
 			this.setImage(Resource.IMG_TELEPORT);
 			this.setText("Teleport");
-			this.cardButton
+			this.cardButtonImage
 					.setToolTipText("<html>This card allows you to  <b>move directly </b> to the Human Sector from any part of the ship.<br>"
 							+ "This is in addition to your normal movement which can happen before or after you use the item.</html>");
 		}
@@ -170,7 +201,7 @@ public class CardButton extends JPanel {
 			Image tempImage = ImageIO.read(new File(pathToImage));
 			Image cardImage = tempImage.getScaledInstance(100, -1,
 					Image.SCALE_SMOOTH);
-			cardButton.setIcon(new ImageIcon(cardImage));
+			cardButtonImage.setIcon(new ImageIcon(cardImage));
 
 		} catch (IOException ex) {
 			logger.error(ex.getMessage());
@@ -214,5 +245,4 @@ public class CardButton extends JPanel {
 		}
 		return null;
 	}
-
 }
